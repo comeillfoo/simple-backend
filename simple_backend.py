5f348fae54fcf722622ca030e3f9846daa9246e6
come1llf00 <rayveleaveme34@inbox.ru>
2022-06-20 02:32:41 +0300

    feat: added simplest socket.io implementation

--- simple_backend.py
+++ simple_backend.py
@@ -1,10 +1,22 @@
 """it is an entry point."""
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 
 app = Flask(__name__)
 # reading a config from the config.py:Config
 app.config.from_object('config.Config')
+
+socketio = SocketIO(app, cors_allowed_origin='*')
+
+
+@socketio.on('message')
+def evaluate(data):
+    """It's the function that should add two numbers."""
+    print(data)
+    first_term = int(data['first_term'])
+    second_term = int(data['second_term'])
+    send(first_term + second_term, broadcast=False)
 
 
 @app.route("/")
